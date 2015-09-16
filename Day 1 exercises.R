e07737a69eb313abd91adf2af28dbc213dedca0e
sodonnell1 <sodonnell1@g.ucla.edu>
2015-09-16 16:10:50 -0700

    Added initial work of day 1 exercises

---
+++ Day 1 exercises.R
@@ -0,0 +1,18 @@
+########################
+#***Mini Exercise 3.2.1
+R<- 0.95
+for(t in 1:ttMax){
+  NN[t+1]<- R*NN[t]
+}
+NN
+NO<-400
+R<-0.99
+geometricGrowthFun<- function(R, NO, ttMax){
+  NN<-matrix(NA, nrow=1, ncol=ttMax+1)
+  NN[1]<-NO
+  for(t in 1:ttMax){
+    NN[t+1]<- R*NN[t]}
+  plot(1:(ttMax+1), NN, lty=2, type='l', xlab='time', ylab='N', col='red')
+}
+geometricGrowthFun(0.95, 10, 10)
+geometricGrowthFun(1.05, 10, 10)
